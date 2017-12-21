bc1ea133ab7ad23f502dcc2852120f168c497b82
Imran M Yousuf <imyousuf@newscred.com>
2017-12-21 12:25:45 +0600

    Add base UI
    Signed-off-by: Imran M Yousuf <imyousuf@newscred.com>

--- web/img-mngr/src/app.js
+++ web/img-mngr/src/app.js
@@ -1,5 +1,12 @@
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
-  }
+    constructor() {
+        this.message = 'Hello World!';
+    }
+
+    configureRouter(config, router) {
+        config.title = 'Images';
+        config.map([
+            { route: '', moduleId: 'root-media', title: 'Root media' }
+        ]);
+    }
 }
---
+++ web/img-mngr/src/root-media.js
@@ -0,0 +1,5 @@
+export class RootMedia {
+    constructor() {
+        this.message = 'Hello world';
+    }
+}
