74763454966a16997747edd94d36915f9c0ce45b
Imran M Yousuf <imyousuf@newscred.com>
2018-03-20 23:53:30 +0600

    Fix breadcrumb UX
    Add the root path to the breadcrumb, also make sure the current path is
    not clickable in the breadkcrumb.
    
    Signed-off-by: Imran M Yousuf <imyousuf@newscred.com>

--- web/img-mngr/src/breadcrumbs.js
+++ web/img-mngr/src/breadcrumbs.js
@@ -6,17 +6,11 @@ export class Breadcrumbs {
     dirs = []
     constructor(ea) {
         this.ea = ea
+        let breadcrumb = this;
         this.ea.subscribe(DirectoryClicked, msg => {
             let dir = msg.directory;
-            let found = false;
-            let foundIndex = -1;
-            for (let index = 0; index < this.dirs.length; ++index) {
-                if (this.dirs[index].ListURI == dir.ListURI) {
-                    found = true
-                    foundIndex = index
-                }
-            }
-            if (!found) {
+            let foundIndex = breadcrumb.findIndex(dir);
+            if (foundIndex <= -1) {
                 this.dirs.push(dir)
             } else {
                 this.dirs.splice(foundIndex + 1, this.dirs.length - foundIndex - 1)
@@ -27,9 +21,28 @@ export class Breadcrumbs {
         });
     }
 
+    findIndex(dir) {
+        let foundIndex = -1;
+        for (let index = 0; index < this.dirs.length; ++index) {
+            if (this.dirs[index].ListURI == dir.ListURI) {
+                foundIndex = index
+            }
+        }
+        return foundIndex;
+    }
+
     clickBreadcrumb(dir) {
-        this.ea.publish(new DirectoryClicked(dir))
-        this.ea.publish(new BreadcrumbClicked(dir))
+        let dirChanged = this.findIndex(dir) + 1 == this.dirs.length;
+        this.ea.publish(new DirectoryClicked(dir));
+        this.ea.publish(new BreadcrumbClicked(dir));
+        if (dirChanged) {
+            return false;
+        }
+        return true;
+    }
+
+    clickHome() {
+        this.ea.publish(new HomeClicked());
         return true;
     }
 }
