fb5b9fa3b2adb17ed9c6bf24b177a7df38fce9bb
Imran M Yousuf <imyousuf@newscred.com>
2018-05-23 20:03:58 +0600

    Add next and previous button for scrolling through images in the dialog
    Signed-off-by: Imran M Yousuf <imyousuf@newscred.com>

--- web/img-mngr/src/components/dialogs/image-detail.js
+++ web/img-mngr/src/components/dialogs/image-detail.js
@@ -2,7 +2,7 @@ import { DialogController } from 'aurelia-dialog';
 
 export class ImageDetail {
     static inject = [DialogController];
-    image = { Name: '', ImageURL: '', ThumbnailURL: '', DownloadID: '' };
+    image = { Name: '', ImageURL: '', ThumbnailURL: '', DownloadID: '', next: null, previous: null };
     constructor(controller) {
         this.controller = controller;
         this.image = null;
@@ -10,5 +10,17 @@ export class ImageDetail {
 
     activate(image) {
         this.image = image;
+    }
+
+    next() {
+        if (this.image.next) {
+            this.image = this.image.next;
+        }
+    }
+
+    previous() {
+        if (this.image.previous) {
+            this.image = this.image.previous;
+        }
     }
 }
