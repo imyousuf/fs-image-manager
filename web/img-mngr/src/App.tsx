62018e686979d620dd8dc83a6365e5342dfce155
Imran M Yousuf <imyousuf@gmail.com>
2020-11-03 15:33:20 -0500

    Setup precise-ui component library to work with the project
    Signed-off-by: Imran M Yousuf <imyousuf@gmail.com>

--- web/img-mngr/src/App.tsx
+++ web/img-mngr/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TextField } from 'precise-ui'
 
 function App() {
   return (
     <div className="App">
+      <TextField label="Label" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
