19ebfd413e91c7e5ead84792cb351be6eb98299a
Imran M Yousuf <imyousuf@gmail.com>
2020-11-03 18:14:31 -0500

    Fix build process for react and precise-ui
    Signed-off-by: Imran M Yousuf <imyousuf@gmail.com>

--- web/img-mngr/src/App.tsx
+++ web/img-mngr/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { TextField } from 'precise-ui'
+import { TextField } from 'precise-ui';
 
 function App() {
   return (
